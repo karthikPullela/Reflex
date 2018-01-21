6ef644ed69b47443830131db204038084172af70
Karthik Pullela <karthik.pullelas@gmail.com>
2018-01-21 03:26:07 -0800

    Added login inermission page.

--- flex/flex/home/urls.py
+++ flex/flex/home/urls.py
@@ -6,4 +6,7 @@ appname = 'home'
 urlpatterns = [
     path('', views.home, name='home'),
     path('signin', views.user_login, name='signin'),
+    path('log', views.log, name='log'),
+    path('dash', views.dash, name='dash'),
+
 ]
--- flex/flex/home/views.py
+++ flex/flex/home/views.py
@@ -10,7 +10,6 @@ import json
 def home(request):
 	module_dir = os.path.dirname(__file__)  # get current directory
 	file_path = os.path.join(module_dir, 'static/home/fit.json')
-	#json_file_name = '../../static/home/fit.json'
 	raw_data = open(file_path, 'rt')
 	my_data = json.load(raw_data)
 	raw_data.close()
@@ -19,8 +18,27 @@ def home(request):
 	while i < len(my_data['bucket']):
 		walk_data.append(my_data['bucket'][i]['dataset'][0]['point'][0]['value'][0]['intVal'])
 		i = i+1
-	#my_data['bucket'][0]['dataset'][0]['point'][0]['value'][0]['intVal']
-	return render(request, 'home/home.html', {'r':walk_data})
+
+	file_path = os.path.join(module_dir, 'static/home/calories.json')
+	raw_data = open(file_path, 'rt')
+	my_data = json.load(raw_data)
+	raw_data.close()
+	calories_data = []
+	i = 0
+	while i < len(my_data['bucket']):
+		calories_data.append(my_data['bucket'][i]['dataset'][0]['point'][0]['value'][0]['fpVal'])
+		i = i+1
+
+	return render(request, 'home/home.html', 
+		{
+		'calories_data':calories_data,
+		'walk_data':walk_data,
+		})
+
+def log(request):
+	return render(request, 'home/login.html')
+
+
 
 def user_login(request):
 	username = 'devam'
@@ -29,7 +47,38 @@ def user_login(request):
 	if auth_user is not None:
 		login(request, auth_user)
 		#return HttpResponse(request.user.username)
-		return redirect('home') # -----------
+		return redirect('dash') # -----------
 	else:
 		return HttpResponse("Unable to log in. User not found. <br>Please check superuser for more details.")
 	return HttpResponse("It's not supposed to come till here.")
+
+
+
+def dash(request):
+
+	module_dir = os.path.dirname(__file__)  # get current directory
+	file_path = os.path.join(module_dir, 'static/home/fit.json')
+	raw_data = open(file_path, 'rt')
+	my_data = json.load(raw_data)
+	raw_data.close()
+	walk_data = []
+	i = 0
+	while i < len(my_data['bucket']):
+		walk_data.append(my_data['bucket'][i]['dataset'][0]['point'][0]['value'][0]['intVal'])
+		i = i+1
+
+	file_path = os.path.join(module_dir, 'static/home/calories.json')
+	raw_data = open(file_path, 'rt')
+	my_data = json.load(raw_data)
+	raw_data.close()
+	calories_data = []
+	i = 0
+	while i < len(my_data['bucket']):
+		calories_data.append(my_data['bucket'][i]['dataset'][0]['point'][0]['value'][0]['fpVal'])
+		i = i+1
+
+	return render(request, 'home/home.html', 
+		{
+		'calories_data':calories_data,
+		'walk_data':walk_data,
+		})
